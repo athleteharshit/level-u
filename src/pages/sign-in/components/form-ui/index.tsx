d3c7ccf15477926879354c7b1e98e4b8500c8620
Harshit gupta <62887518+athleteharshit@users.noreply.github.com>
2022-08-02 23:17:04 +0530

    update

--- src/pages/sign-in/components/form-ui/index.tsx
+++ src/pages/sign-in/components/form-ui/index.tsx
@@ -3,7 +3,7 @@ import { Box } from '@mui/material';
 function FormUi() {
   return (
     <Box>
-      Typescript FormUi
+      Typescript FormU
       {/* <List /> */}
       {/* <Person
         name="harshit"
