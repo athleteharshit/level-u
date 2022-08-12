438dd0024558ab1568c214e04a98e4f13f315e73
Harshit gupta <62887518+athleteharshit@users.noreply.github.com>
2022-08-12 11:12:04 +0530

    update

--- src/pages/sign-in/components/form-ui/index.tsx
+++ src/pages/sign-in/components/form-ui/index.tsx
@@ -1,6 +1,10 @@
 import { Box } from '@mui/material';
 
 function FormUi() {
+  // Write a function called productOfArray which takes in an array of numbers and returns the product of them all
+  // var six = productOfArray([1,2,3]) // 6
+  // var sixty = productOfArray([1,2,3,10]) // 60
+
   return (
     <Box>
       Typescript FormU
