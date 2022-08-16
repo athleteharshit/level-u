85ab94646f7b4632e5d821795dcf9814fcd08f68
athleteharshit <guptaharshit545@gmail.com>
2022-08-16 22:53:34 +0530

    update

--- src/pages/sign-in/components/form-ui/index.tsx
+++ src/pages/sign-in/components/form-ui/index.tsx
@@ -5,6 +5,7 @@ function FormUi() {
   // Write a function called productOfArray which takes in an array of numbers and returns the product of them all
   // var six = productOfArray([1,2,3]) // 6
   // var sixty = productOfArray([1,2,3,10]) // 60
+
   return (
     <Box>
       Typescript FormU {Env.BASE_URL}
---
+++ src/services/index.ts
@@ -0,0 +1,5 @@
+const apiType = {
+  get: 'get'
+};
+
+export default apiType;
